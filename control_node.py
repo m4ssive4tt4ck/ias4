eca197b7fe6ae4ee7909e00d1d19e702c84c14d1
m4ssive4tt4ck <rahelkempf@gmx.ch>
2022-04-21 17:41:00 +0200

    initial idea

--- control_node.py
+++ control_node.py
@@ -1,30 +1,43 @@
 import sys
 import socket
-import select
+import json
 
 #pure tcp sender 
-def start_connection(HOST, PORT):
+def send_message():
+    HOST = str(sys.stdin.readline("Who should be the source of the message")) #first one to receive the message, as control_node is only for testing (IP)
+    PORT = 65432
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     server.bind((socket.gethostname(), 54322)) #TODO: gethostname überprüfen 
     server.connect((HOST, PORT))
     while True:
-        message = sys.stdin.readline()
-        #VORGABE EINGABE: targetNodeName message
-        #TODO: add sourceNode to message 
+        message = sys.stdin.readline("format: targetNodeName message")
+        message = HOST + message #VORGABE EINGABE: HOST(=sourceNode) targetNodeName message TODO: use nodename instead of IP 
         server.send(message.encode('UTF-8'))
         server.close() #close connection directly after sending message
 
-#TODO: send NTU to all nodes 
-#knows all connections (all ips)
+def setup_network_topology():
+    file = open('network_topology_1.json') 
+    network_topology = json.load(file) #TODO: make global variable
+    allnodes = ""
 
-#For simplicity, the network topology is read-in from a file in a simplistic raw format.
-#In other words, it can be a list of node-names with IPs, and a list of node-names with
-#connections to some other node-names, each with an integer number for its RTT. The
-#exact format can be chosen freely.
+    for node in network_topology['nodelist']:
+        allnodes += node['nodename'] + " "
+
+    for node in network_topology['nodelist']:
+        print(node)
+        #send nodelist with nodenames only + whole object of node to node 
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        server.bind((socket.gethostname(), 54322)) #TODO: gethostname überprüfen 
+        server.connect((node['ip'], 65432)) #TODO: woher weiss ich den port ... 
+        while True:
+            server.send("__SETUP", node.encode('UTF-8'), allnodes.encode('UTF-8') ) #TODO: check
+            server.close() #close connection directly after sending message 
+
+#TODO: method to give new file to setup_network_topology(file)
 
 if __name__ == '__main__':
+    setup_network_topology() 
+
     # checks whether sufficient arguments have been provided
-    if len(sys.argv) != 3:
-        print("Correct usage: script, Host IP address, port number")
-        exit()
-    start_connection(str(sys.argv[1]), str(sys.argv[2]))
+    send_message()
--- peer_node.py
+++ peer_node.py
@@ -3,33 +3,41 @@ import sys
 
 def start_receiver(HOST, PORT):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    server.bind((HOST, PORT)) 
+    server.bind((socket.gethostname(), 65432)) #TODO: handle ports 
     server.listen(30) #listens for 30 active connections
-   
+
     while True: 
-        conn, addr = server.accept()
-        splitMessage;  
+        conn, addr = server.accept() 
         with conn: 
-            #TODO: add connection to known connections 
             message = conn.recv(2048).decode('UTF-8')
-            splitMessage = message.split(" ", 2) #PACKET DEFINITION: packet = sourceIP targetNode rest:message 
-            if(splitMessage[1] == HOST): #target is this machine --> print message 
-                print(splitMessage[0], ": ", splitMessage[2])
-            conn.close()
-           
-        #TODO: check forwarding table and what to do if not found 
-        server.connect((splitMessage[1], PORT)) #TODO: handle different ports 
-        #TODO:send to other connection ... server.send() --> connection muss erst bestehen
-        server.send(message.encode('UTF-8'))
-        print("forward")
-           
-        # server.close()
+            if (message.startswith('__SETUP')):
+                conn.close() #TODO: check if setup_node wird ausgeführt
+                setup_node(message)
+            else: 
+                splitMessage = message.split(" ", 2) #PACKET DEFINITION: packet = sourceIP targetNode rest:message 
+                if(splitMessage[1] == HOST): #target is this machine --> print message 
+                    print(splitMessage[0], ": ", splitMessage[2])
+                    conn.close()
+                else: 
+                    conn.close() #TODO: check if forward message noch aufgerufen wird 
+                    print("forward")
+                    forward_message(message) 
 
-#TODO: NTU etc... 
+def forward_message(message):
+    #TODO: check connections of this node -> see who it has to be delivered to 
+    #get ip if in forwarding table 
+    # server.connect((splitMessage[1], PORT)) #TODO: handle different ports 
+    # server.send(message.encode('UTF-8'))
+    NotImplemented
 
+def setup_node(message):
+    #TODO: message aufsplitten (node + connections, nodelist, ...) 
+    #TODO: forwarding TABLE !!! einrichten 
+    NotImplemented
+    
 if __name__ == '__main__':
    # checks whether sufficient arguments have been provided
     if len(sys.argv) != 3: 
         print("Correct usage: script, IP address, port number")
         exit()
-    start_server(str(sys.argv[1]), int(sys.argv[2])) #host ip and port are given via command line 
+    start_receiver(str(sys.argv[1]), int(sys.argv[2])) #host ip and port are given via command line 
