320b7d4b879736c75862eeaa3c7a1ec4d4f9966e
fahrni <84290462+Joreyn@users.noreply.github.com>
2022-04-22 17:23:42 +0200

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -1,29 +1,37 @@
 import numpy as np
-
+import pandas as pd
 
 cost_array = None
 
 
-# update has form [final destination: str, ]
+# cost_array[0, 0] = command
+# cost_array[1, 0] = message
+# cost_array[:, 1] = destination names
+# cost_array[:, 2] = path cost
+# cost_array[:, 3] = next addressee
+# cost_array[:, 4] = addressee address
+
+
 def bellman_ford(final_destination: str, cost: int, sender_name: str) -> None:
-    destination_index = np.argwhere(cost_array[:, 0] == final_destination)
+    destination_index = np.argwhere(cost_array[:, 1] == final_destination)
 
-    sender_index = np.argwhere(cost_array[:, 0] == sender_name)
-    sender_cost = int(cost_array[sender_index, 1])
-    new_cost = cost+sender_cost
+    sender_index = np.argwhere(cost_array[:, 1] == sender_name)
+    sender_cost = int(cost_array[sender_index, 2])
+    new_cost = cost + sender_cost
 
-    new_cost_smaller = int(cost_array[destination_index, 1]) > new_cost
-    no_connection_yet = int(cost_array[destination_index, 1]) < 0
+    new_cost_smaller = int(cost_array[destination_index, 2]) > new_cost
+    no_connection_yet = int(cost_array[destination_index, 2]) < 0
 
     if no_connection_yet or new_cost_smaller:
-        cost_array[destination_index, 1] = cost + sender_cost
-        cost_array[destination_index, 2] = sender_name
+        cost_array[destination_index, 2] = cost + sender_cost
+        cost_array[destination_index, 3] = sender_name
         # TODO send to all neighbors
 
 
 if __name__ == '__main__':
-    cost_list = [["Node1", 0, "Node1"], ["Node2", 5, "Node2"], ["Node3", 2, "Node3"], ["Node4", -1, ""]]
+    cost_list = [["command", "Node1", 0, "Node1", "192.7123912"], ["message", "Node2", 5, "Node2", ".102837"],
+                 ["", "Node3", 2, "Node3", ""], ["", "Node4", -1, "", ""]]
+    cost_list.index()
     cost_array = np.array(cost_list)
     print(cost_array)
     bellman_ford("Node2", 1, "Node3")
-    print(cost_array)
