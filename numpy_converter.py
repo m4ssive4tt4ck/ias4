33e72c1e78d9f74cc3f51c6980acbb076335a905
fahrni <84290462+Joreyn@users.noreply.github.com>
2022-04-24 17:29:46 +0200

    asdfgh

---
+++ numpy_converter.py
@@ -0,0 +1,75 @@
+import numpy as np
+# from io import BytesIO
+# import pickle
+
+
+# # not needed, for documentation only
+# def from_array_to_byte(np_array):
+#     bytes_ = BytesIO()
+#     np.save(bytes_, np_array, allow_pickle=True)
+#     return bytes_.getvalue()
+#
+#
+# # not needed, for documentation only
+# def from_byte_to_array(bytes_):
+#     bytes_ = BytesIO(bytes_)
+#     np_array = np.load(bytes_, allow_pickle=True)
+#     return np_array
+#
+#
+# # not needed, for documentation only
+# def from_array_to_pickle(np_array):
+#     pickled = pickle.dumps(np_array)
+#     return pickled
+#
+#
+# # not needed, for documentation only
+# def from_pickle_to_array(pickled):
+#     np_array = pickle.loads(pickled)
+#     return np_array
+
+
+# converts a numpy array to a string, with "," and ";" as separators
+# input: numpy array
+# output: string
+def array_to_string(array: np.ndarray):
+    as_string = ""
+    for part_array in array:
+        for element in part_array:
+            as_string += str(element) + ","
+        as_string = as_string[0:-1]+";"
+    as_string = as_string[0:-1]
+    print(as_string)
+    return as_string
+
+
+# converts a properly formatted string to a numpy array
+# input: string
+# output: numpy array
+def string_to_array(message: str):
+    array_as_list = message.split(";")
+    print(array_as_list)
+    for i in range(len(array_as_list)):
+        array_as_list[i] = array_as_list[i].split(",")
+    array = np.array(array_as_list, dtype=str)
+    print(array)
+    return array
+
+
+if __name__ == '__main__':
+    array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print(array1)
+    match string_to_array(array_to_string(array1))[0,0]:
+        case "1":
+            print("case1")
+    # print(array)
+    # bytes_2 = from_array_to_byte(array)
+    # print(bytes_2)
+    # decoded_array = from_byte_to_array(bytes_2)
+    # print(decoded_array)
+    #
+    # pickled = from_array_to_pickle(array)
+    # print(pickled)
+    # decoded_array2 = from_pickle_to_array(pickled)
+    # print(decoded_array2)
+
--- shortest_path.py
+++ shortest_path.py
@@ -1,37 +1,40 @@
 import numpy as np
 import pandas as pd
+import numpy_converter as np_c
+
 
 cost_array = None
 
 
-# cost_array[0, 0] = command
-# cost_array[1, 0] = message
-# cost_array[:, 1] = destination names
-# cost_array[:, 2] = path cost
-# cost_array[:, 3] = next addressee
-# cost_array[:, 4] = addressee address
+# cost_array[0, :] = Nothing (allows for easier indexing)
+# cost_array[1, :] = destination names
+# cost_array[2, :] = path cost
+# cost_array[3, :] = next addressee
+# cost_array[4, :] = addressee address
 
 
 def bellman_ford(final_destination: str, cost: int, sender_name: str) -> None:
-    destination_index = np.argwhere(cost_array[:, 1] == final_destination)
+    destination_index = np.argwhere(cost_array[1, :] == final_destination)
 
-    sender_index = np.argwhere(cost_array[:, 1] == sender_name)
-    sender_cost = int(cost_array[sender_index, 2])
+    sender_index = np.argwhere(cost_array[1, :] == sender_name)
+    sender_cost = int(cost_array[2, sender_index])
     new_cost = cost + sender_cost
 
-    new_cost_smaller = int(cost_array[destination_index, 2]) > new_cost
-    no_connection_yet = int(cost_array[destination_index, 2]) < 0
+    new_cost_smaller = int(cost_array[2, destination_index]) > new_cost
+    no_connection_yet = int(cost_array[2, destination_index]) < 0
 
     if no_connection_yet or new_cost_smaller:
-        cost_array[destination_index, 2] = cost + sender_cost
-        cost_array[destination_index, 3] = sender_name
+        cost_array[2, destination_index] = cost + sender_cost
+        cost_array[3, destination_index] = sender_name
         # TODO send to all neighbors
 
 
 if __name__ == '__main__':
     cost_list = [["command", "Node1", 0, "Node1", "192.7123912"], ["message", "Node2", 5, "Node2", ".102837"],
                  ["", "Node3", 2, "Node3", ""], ["", "Node4", -1, "", ""]]
-    cost_list.index()
-    cost_array = np.array(cost_list)
+    cost_array = np.array(cost_list).T
     print(cost_array)
     bellman_ford("Node2", 1, "Node3")
+    print(cost_array)
+    np_c.string_to_array(np_c.array_to_string(cost_array))
+
