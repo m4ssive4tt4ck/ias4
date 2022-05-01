0f6605218b4ab1d6db6780baec63f5c9ea2575cb
m4ssive4tt4ck <rahelkempf@gmx.ch>
2022-05-01 10:45:41 +0200

    send pendiong

--- peer_node.py
+++ peer_node.py
@@ -47,7 +47,7 @@ def start_receiver(HOST, PORT):
                 initialize(message_array)
                 print("newly initialized")
                 if message_array[0, 2] == "final":
-                    send_pending(server)
+                    send_pending(HOST, PORT)
                     # TODO send update to all
             elif message_array[0, 0] == "update":
                 update(message_array)
